aa32a78208d0ddf5e10309207da252921822518b
aiden30015 <loolloo080313@gmail.com>
2025-06-26 19:31:49 +0900

    💄 :: 메인 화면 퍼블리싱 완료

---
+++ lib/app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_clone/component/login_button.dart';
+
+class SplashScreen extends HookConsumerWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.grey[900]!,
+              Colors.black,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
+            child: Column(
+              children: [
+                const Spacer(),
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: SvgPicture.asset('assets/svg/spotify_logo.svg'),
+                ),
+                const SizedBox(height: 20),
+                const FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    '수백만 곡을 들을 수 있습니다.\nSpotify에서 무료로 제공합니다.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const Spacer(),
+                LoginButton(text: '무료 가입', onTap: () {}, signup: true),
+                const SizedBox(height: 10),
+                LoginButton(text: '로그인', onTap: () {}, signup: false),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
