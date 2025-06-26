1ab0ca305bca7d874c68d613d63d2101e9999e25
aiden30015 <loolloo080313@gmail.com>
2025-06-26 19:31:17 +0900

    💄 :: 로그인 및 회원가입 공통 컴포넌트 퍼블리싱 완료

---
+++ lib/component/login_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class LoginButton extends StatelessWidget {
+
+  final String text;
+  final VoidCallback onTap;
+  final bool signup;
+  final SvgPicture? icon;
+
+  const LoginButton({super.key, required this.text, required this.onTap, required this.signup,this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 337,
+        height: 49,
+        decoration: BoxDecoration(
+          color: signup ? Colors.green : null,
+          borderRadius: BorderRadius.circular(45),
+          border: signup ? null : Border.all(color: Colors.white),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (signup)
+              icon ?? SizedBox.shrink(),
+            Text(
+              text,
+              style: TextStyle(
+                color: signup ? Colors.black : Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
