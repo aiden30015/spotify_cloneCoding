992c7d13ccf5d0803320cc18095015157c215908
aiden30015 <loolloo080313@gmail.com>
2025-07-01 16:14:26 +0900

    🔧 :: 파일 위치 변경

---
+++ lib/routers.dart
@@ -0,0 +1,37 @@
+import 'package:go_router/go_router.dart';
+import 'package:spotify_clone/app.dart';
+import 'package:spotify_clone/presentations/signup/screens/email_signup_screen.dart';
+import 'package:spotify_clone/presentations/signup/screens/signup_screen.dart';
+
+// 라우트 이름 상수
+class AppRoutes {
+  static const String splash = '/';
+  static const String signup = '/signup';
+  static const String emailSignup = '/emailSignup';
+}
+
+// 메인 라우터 설정
+final router = GoRouter(
+  initialLocation: AppRoutes.splash,
+  routes: [
+    // 스플래시 화면
+    GoRoute(
+      path: AppRoutes.splash,
+      name: 'splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
+    
+    
+    // 회원가입 화면
+    GoRoute(
+      path: AppRoutes.signup,
+      name: 'signup',
+      builder: (context, state) => const SignupScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.emailSignup,
+      name: 'emailSignup',
+      builder: (context, state) => const EmailSignupScreen(),
+    )
+  ],
+);
